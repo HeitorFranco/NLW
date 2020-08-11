348d388499eae8edebcb7671d4fd73f6a0eab6b1
HeitorFranco <53319380+HeitorFranco@users.noreply.github.com>
2020-08-11 04:29:19 -0300

    Update db.js

--- src/database/db.js
+++ src/database/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize").Sequelize
 
 
-const sequelize = new Sequelize('db_ecoleta', 'heitormaf@187.84.225.36', 'y9#qjjS4KF3sSD2', {
+const sequelize = new Sequelize('db_ecoleta', 'heitormaf', 'y9#qjjS4KF3sSD2', {
     host: 'mysql669.umbler.com',
     dialect: 'mysql'
 });
