f878f965f4c1aaabe1b613a5df71d2cfad6a58ec
HeitorMaf <heitorfranco04@gmail.com>
2020-09-03 01:33:16 -0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -17,4 +17,4 @@ server
 .get("/create-point", pageCreatePoint)
 .post("/add", pageRegisterPoint)
 .get("/search-results", pageSearchResults)
-.listen(8080, () => console.log("server rodando em http://localhost:8080"))
+.listen(5000, () => console.log("server rodando em http://localhost:5000"))
