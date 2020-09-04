1a457ee2d0ca691d1a47396a3200e7fcafff8fe3
HeitorMaf <heitorfranco04@gmail.com>
2020-09-03 21:42:57 -0300

    all points

--- src/pages.js
+++ src/pages.js
@@ -7,21 +7,39 @@ function pageCreatePoint(req, res){
     return res.render("create-point.html")
 }
 function pageSearchResults(req, res){
-   
-    point.findAll({where: {cidade: req.query.search}}).then((cards) => {
-        const itens = ["Lâmpadas", "Pilhas e Baterias", "Papéis e Papelão", "Resíduos Eletrônicos", "Resíduos Orgânicos", "Óleo de Cozinha"]
-        for(let card of cards){
-            card.itensdecoleta = String(card.itensdecoleta).split(",")
-            novoArray = []
-            card.itensdecoleta.forEach((element, index) => {
-                novoArray.push(itens[element-1]) 
-                if(index+1 != card.itensdecoleta.length) {novoArray[index]+=", "}
-            });
-            card.itensdecoleta = novoArray
-            console.log(card.itensdecoleta)
-        }
-        res.render("search-results.html", {cards})
-    }).catch((error) => console.log(error))
+    if(!req.query.search){
+        point.findAll().then((cards) => {
+            const itens = ["Lâmpadas", "Pilhas e Baterias", "Papéis e Papelão", "Resíduos Eletrônicos", "Resíduos Orgânicos", "Óleo de Cozinha"]
+            for(let card of cards){
+                card.itensdecoleta = String(card.itensdecoleta).split(",")
+                novoArray = []
+                card.itensdecoleta.forEach((element, index) => {
+                    novoArray.push(itens[element-1]) 
+                    if(index+1 != card.itensdecoleta.length) {novoArray[index]+=", "}
+                });
+                card.itensdecoleta = novoArray
+                console.log(card.itensdecoleta)
+            }
+            return res.render("search-results.html", {cards})
+        }).catch((error) => console.log(error))
+    }else{
+        point.findAll({where: {cidade: req.query.search}}).then((cards) => {
+            const itens = ["Lâmpadas", "Pilhas e Baterias", "Papéis e Papelão", "Resíduos Eletrônicos", "Resíduos Orgânicos", "Óleo de Cozinha"]
+            for(let card of cards){
+                card.itensdecoleta = String(card.itensdecoleta).split(",")
+                novoArray = []
+                card.itensdecoleta.forEach((element, index) => {
+                    novoArray.push(itens[element-1]) 
+                    if(index+1 != card.itensdecoleta.length) {novoArray[index]+=", "}
+                });
+                card.itensdecoleta = novoArray
+                console.log(card.itensdecoleta)
+            }
+            res.render("search-results.html", {cards})
+        }).catch((error) => console.log(error))
+    }
+
+    
 }
 
 function pageRegisterPoint(req, res) {
