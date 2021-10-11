8b995de30c245514f2fad4d83ef516777797b370
HeitorFranco <heitorfranco04@gmail.com>
2021-10-11 19:51:00 -0300

    alterando a conexão do banco de dados e protegendo os dados do banco de dados

--- src/database/db.js
+++ src/database/db.js
@@ -1,23 +1,28 @@
-const Sequelize = require("sequelize").Sequelize
+const Sequelize = require("sequelize").Sequelize;
 
-
-const sequelize = new Sequelize('bwamu85fha8xxuasmoav', 'u0ge2s4wc4aag1hr', 'T6jlA3lG0RKdUdIaMlm2', {
-    host: 'bwamu85fha8xxuasmoav-mysql.services.clever-cloud.com',
-    dialect: 'mysql'
+var sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
+
 /*const sequelize = new Sequelize('pontosdecoleta', 'root', '123456', {
     host: 'localhost',
     dialect: 'mysql'
 });*/
 async function testConection() {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-      } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 }
 
-testConection()
+testConection();
 
-module.exports = sequelize
+module.exports = sequelize;
--- src/server.js
+++ src/server.js
@@ -1,20 +1,33 @@
-const express = require("express")
-const server = express()
-const { pageHome, pageCreatePoint, pageSearchResults, pageRegisterPoint, pagePoint } = require("./pages")
+const express = require("express");
 
-const nunjucks = require("nunjucks")
-const PORT = process.env.PORT || 5000
+const dotenv = require("dotenv");
+dotenv.config();
+
+const server = express();
+const {
+  pageHome,
+  pageCreatePoint,
+  pageSearchResults,
+  pageRegisterPoint,
+  pagePoint,
+} = require("./pages");
+
+const nunjucks = require("nunjucks");
+const PORT = process.env.PORT || 5000;
+
 nunjucks.configure("src/views", {
-    express: server,
-    noCache: true
-})
+  express: server,
+  noCache: true,
+});
 
-server.use(express.urlencoded( { extended: false } ))
-server.use(express.static("public"))
+server.use(express.urlencoded({ extended: false }));
+server.use(express.static("public"));
 
 server
-.get("/", pageHome)
-.get("/create-point", pageCreatePoint)
-.post("/add", pageRegisterPoint)
-.get("/search-results", pageSearchResults)
-.listen(PORT, () => console.log(`server rodando em http://localhost:${PORT}`))
+  .get("/", pageHome)
+  .get("/create-point", pageCreatePoint)
+  .post("/add", pageRegisterPoint)
+  .get("/search-results", pageSearchResults)
+  .listen(PORT, () =>
+    console.log(`server rodando em http://localhost:${PORT}`)
+  );
